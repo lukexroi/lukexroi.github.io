46423c64772cf8882d569461187c36c335de07d3
Lukeroy17 <2102355A@student.tp.edu.sg>
2023-04-02 02:47:02 +0800

    minor changes to greeting

--- src/containers/greeting/Greeting.jsx
+++ src/containers/greeting/Greeting.jsx
@@ -11,26 +11,24 @@ export default function Greeting(props) {
         <div className="site__greeting">
             <div className="site__greeting-main">
                 <div className="site__greeting-main-text-div">
-                    <div>
-                        <h1 className="greeting-text">Hey There, I'm Luke 👋🏻</h1>
-                        <h2 className="greeting-nickname">Poh Jie Ren Luke</h2>
-                        <h2 className="greeting-text-h2 subTitle">
-                            <Typed
-                                strings={[
-                                    "Junior in Diploma of Applied Artificial Intelligence (AAI)",
-                                    "Temasek Polytechnic (TP) Scholar",
-                                    "Vice President of AAI Student Interest Group",
-                                    "LEAD EXCEL Student (Top 1% of TP's Leadership Programme)",
-                                    "Co-founder of You'th",
-                                    "Member of Health Promotion Board's Youth Resource Panel (HPB YRP)"
-                                ]}
-                                typeSpeed={50}
-                                backSpeed={25}
-                                loop
-                            />
-                        </h2>
-                        <SocialMedia theme={theme} />
-                    </div>
+                    <h1 className="greeting-text">Hey, I'm Luke 👋🏻</h1>
+                    <h2 className="greeting-nickname">Poh Jie Ren Luke</h2>
+                    <h2 className="greeting-text-h2 subTitle">
+                        <Typed
+                            strings={[
+                                "Junior in Diploma of Applied Artificial Intelligence (AAI)",
+                                "Temasek Polytechnic (TP) Scholar",
+                                "Vice President of AAI Student Interest Group",
+                                "LEAD EXCEL Student (Top 1% of TP's Leadership Programme)",
+                                "Co-founder of You'th",
+                                "Member of Health Promotion Board's Youth Resource Panel (HPB YRP)"
+                            ]}
+                            typeSpeed={50}
+                            backSpeed={25}
+                            loop
+                        />
+                    </h2>
+                    <SocialMedia theme={theme} />
                 </div>
                 <div className="site__greeting-main-image-div">
                     <img
