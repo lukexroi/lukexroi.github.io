25623b6a3c95ef1cbe14f8567043bb7e938fb36d
Lukeroy17 <2102355A@student.tp.edu.sg>
2023-04-02 18:19:21 +0800

    minor adjustment to vertical timeline element order

--- src/portfolio.js
+++ src/portfolio.js
@@ -39,26 +39,15 @@ const socialMediaLinks = [
 
 const pathwayDetails = [
     {
-        date: "2017 - 2020",
+        date: "2026 September - 2030 April",
         iconBackgroundColor: "#3e497a",
         iconColor: "#fff",
-        icon: <SchoolIcon/>,
-        title: "Anglo Chinese School Independent (ACSI)",
-        subtitle: "The Singapore-Cambridge General Certificate of Education Ordinary Level (GCE O-Level)",
-        description1: "L1R5 Raw Score of 13, Nett Score of 11",
-        description2: "L1R4/EL2BR2 Raw Score of 10, Nett Score of 8 ",
-        description3: ""
-    },
-    {
-        date: "2021 April - 2022 March",
-        iconBackgroundColor: "#3e497a",
-        iconColor: "#fff",
-        icon: <SchoolIcon/>,
-        title: "Temasek Polytechnic (TP)",
-        subtitle: "Common Information and Communications Technology Programme (CICT)",
-        description1: "cGPA of AY2021/2022 of 3.88",
-        description2: "AY2021/2022 April Semester (Main) GPA of 4.00",
-        description3: "AY2021/2022 October Semester (Main) GPA of 3.76",
+        icon: <ProgressIcon/>,
+        title: "Singapore University of Technology and Design (SUTD)",
+        subtitle: "Diploma in Applied Artificial Intelligence",
+        description1: "Successful Applicant of SUTD's Early Engagement & Development (SEED) Programme ",
+        description2: "Conditional Offer to SUTD",
+        description3: "Aspiring to enroll in SUTD's Undergraduate Programme of Design and Artificial Intelligence (DAI)",
     },
     {
         date: "2022 April - 2024 March",
@@ -72,15 +61,26 @@ const pathwayDetails = [
         description3: "AY2022/2023 October Semester (Main) GPA of 4.00",
     },
     {
-        date: "2026 September - 2030 April",
+        date: "2021 April - 2022 March",
         iconBackgroundColor: "#3e497a",
         iconColor: "#fff",
-        icon: <ProgressIcon/>,
-        title: "Singapore University of Technology and Design (SUTD)",
-        subtitle: "Diploma in Applied Artificial Intelligence",
-        description1: "Successful Applicant of SUTD's Early Engagement & Development (SEED) Programme ",
-        description2: "Conditional Offer to SUTD",
-        description3: "Aspiring to enroll in SUTD's Undergraduate Programme of Design and Artificial Intelligence (DAI)",
+        icon: <SchoolIcon/>,
+        title: "Temasek Polytechnic (TP)",
+        subtitle: "Common Information and Communications Technology Programme (CICT)",
+        description1: "cGPA of AY2021/2022 of 3.88",
+        description2: "AY2021/2022 April Semester (Main) GPA of 4.00",
+        description3: "AY2021/2022 October Semester (Main) GPA of 3.76",
+    },
+    {
+        date: "2017 - 2020",
+        iconBackgroundColor: "#3e497a",
+        iconColor: "#fff",
+        icon: <SchoolIcon/>,
+        title: "Anglo Chinese School Independent (ACSI)",
+        subtitle: "The Singapore-Cambridge General Certificate of Education Ordinary Level (GCE O-Level)",
+        description1: "L1R5 Raw Score of 13, Nett Score of 11",
+        description2: "L1R4/EL2BR2 Raw Score of 10, Nett Score of 8 ",
+        description3: ""
     }
 ];
 
